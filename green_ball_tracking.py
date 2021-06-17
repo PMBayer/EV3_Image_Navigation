30c72f76f0932d201d1727518d9d9bf3c214adcc
pmbayer <Pasybayer123>
2021-06-17 11:47:23 +0200

    commit

--- green_ball_tracking.py
+++ green_ball_tracking.py
@@ -3,6 +3,7 @@
 import argparse
 import time
 from collections import deque
+
 import cv2
 import imutils
 import numpy as np
@@ -57,15 +58,16 @@ while True:
 
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+    mask_contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    mask_contours = imutils.grab_contours(mask_contours)
     center = None
+
     # only proceed if at least one contour was found
-    if len(cnts) > 0:
+    if len(mask_contours) > 0:
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
-        c = max(cnts, key=cv2.contourArea)
+        c = max(mask_contours, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -74,8 +76,7 @@ while True:
         if radius > 10:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-                       (0, 255, 255), 2)
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
     # update the points queue
